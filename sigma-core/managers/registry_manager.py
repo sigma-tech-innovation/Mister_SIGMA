1f5ac01cb1fc0ab841260ca380578652e6b4a8ce
Ayoub El Belkasmi <founder@sigma-tech-innovation.com>
2026-07-06 07:10:08 +0100

    SIG-CORE: add registry manager

---
+++ sigma-core/managers/registry_manager.py
@@ -0,0 +1,31 @@
+import importlib.util
+from pathlib import Path
+
+spec = importlib.util.spec_from_file_location(
+    "base_manager",
+    Path.cwd() / "sigma-core/managers/base_manager.py"
+)
+
+base = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(base)
+
+class RegistryManager(base.BaseManager):
+
+    def list(self):
+        for reg in self.database.load("registry"):
+            print(
+                f'{reg["id"]} | {reg["name"]} | {reg["version"]}'
+            )
+
+    def next_id(self):
+        db = self.database.load("registry")
+
+        if not db:
+            return "REG-0001"
+
+        last = max(
+            int(x["id"].split("-")[1])
+            for x in db
+        )
+
+        return f"REG-{last+1:04d}"
