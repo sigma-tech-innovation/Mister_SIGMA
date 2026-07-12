645a21ee64e579ac39800f7410ba2a649880c1b4
Ayoub El Belkasmi <founder@sigma-tech-innovation.com>
2026-07-12 20:20:37 +0100

    SIG-PLUGINS: add plugin disable

--- sigma-core/managers/plugin_manager.py
+++ sigma-core/managers/plugin_manager.py
@@ -16,9 +16,9 @@ class PluginManager(base.BaseManager):
     def save(self,data):
         self.database.save("plugins",data)
 
-    def enable(self,name):
+    def disable(self,name):
         db=self.db()
         for p in db:
             if p["name"]==name:
-                p["status"]="enabled"
+                p["status"]="disabled"
         self.save(db)
